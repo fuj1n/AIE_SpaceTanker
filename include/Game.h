94944dca0e9002a408c4689280f97b88a0ddab45
Arthur Uzulin <fuj1nman@hotmail.com>
2014-03-18 18:18:47 +1100

    Added the missing header

---
+++ include/Game.h
@@ -0,0 +1,31 @@
+#pragma comment(lib, "winmm") 
+
+unsigned int splashTexture;
+unsigned int instructionTexture;
+unsigned int menuButtons[4];
+
+unsigned int stars;
+
+class IDrawable{
+public: 
+	virtual unsigned int getTexture() = 0;
+	virtual void update() = 0;
+};
+
+class ICollidable{
+public:
+	virtual unsigned int getX() = 0;
+	virtual unsigned int getY() = 0;
+	virtual unsigned int getWidth() = 0;
+	virtual unsigned int getHeight() = 0;
+	virtual bool isCollideTester() = 0;
+	virtual void onCollide(ICollidable*) = 0;
+	virtual void onTesterMessage(ICollidable*) = 0;
+
+	virtual char* getColliderName() = 0;
+};
+
+class ITrackable{
+	virtual unsigned int getX() = 0;
+	virtual unsigned int getY() = 0;
+};
