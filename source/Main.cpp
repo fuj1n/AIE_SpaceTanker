e793d5b618796085890b5982eb0c4c12965a6ea5
Arthur Uzulin <fuj1nman@hotmail.com>
2014-04-10 18:33:52 +1000

    Fixed the arguments

--- source/Main.cpp
+++ source/Main.cpp
@@ -6,9 +6,12 @@
 int main(int argc, char* argv[]){
 	int displayWidth = 640, displayHeight = 480;
 	bool displayFScreen = false;
-	for(int i = 0; i < argc; i++){
-		char* arg = argv[i];
+
+	for(unsigned int i = 1; i < argc; i++){
+		std::string arg = std::string(argv[i]);
 		
+		std::cout << arg << std::endl;
+
 		if(i + 1 < argc){
 			if(arg == "-dispWidth"){
 				displayWidth = atoi(argv[i + 1]);
@@ -20,15 +23,15 @@ int main(int argc, char* argv[]){
 		if(arg == "-fscreen"){
 			displayFScreen = true;
 		}
-
-		Application* appl = new Application(displayWidth, displayHeight, displayFScreen);
-
-		int exitCode = appl->run();
-		
-		//Causes a debug assertion failure
-		//delete appl;
-
-		return exitCode;
 	}
+
+	Application* appl = new Application(displayWidth, displayHeight, displayFScreen);
+
+	int exitCode = appl->run();
+
+	//Causes a debug assertion failure
+	//delete appl;
+
+	return exitCode;
 
 }
